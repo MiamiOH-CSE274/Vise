6eae01a0a1dc6d6a224f61511ad584ed3484273b
continnd <continnd@miamioh.edu>
2013-11-30 16:53:05 -0500

    resolved conflicts

--- src/gameBoard.h
+++ src/gameBoard.h
@@ -7,38 +7,13 @@
  * using vector for both the main array and for the actual adjacency "lists"
  */
 
-<<<<<<< HEAD
 #include <vector>
 
-=======
-
-#include <vector>
-
-
->>>>>>> 06038faed0461eb65471033a31040843ed01f523
 //The move class has only a cost and a destination.
 class possibleMove{
 public:
   possibleMove(int d){dest = d;};
   possibleMove(){dest = -1;};
-
-<<<<<<< HEAD
-  int dest; //Destination node number
-};
-
-class hexSpace{
-public:
-	hexSpace(){type=0;};
-	int type;
-	hexSpace* right;
-	hexSpace* left;
-	hexSpace* upright;
-	hexSpace* upleft;
-	hexSpace* downright;
-	hexSpace* downleft;
-	std::vector<possibleMove>moveList;
-};
-=======
 
   int dest; //Destination node number
 };
@@ -56,7 +31,3 @@ public:
         hexSpace* downleft;
         std::vector<possibleMove>moveList;
 };
-
-
-
->>>>>>> 06038faed0461eb65471033a31040843ed01f523
